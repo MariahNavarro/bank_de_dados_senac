2970fb4fdd594595bc2fff00dfcf5f978bf32813
MariahNavarro <mariahaparecida112@gmail.com>
2025-11-18 18:47:42 -0300

    Update bdEscola.sql

--- bdEscola.sql
+++ bdEscola.sql
@@ -19,7 +19,6 @@ CREATE TABLE Turmas (
     CursoID INT NOT NULL,
     ProfessorID INT NOT NULL,
     AnoLetivo YEAR NOT NULL,
- 
     FOREIGN KEY (CursoID) REFERENCES Cursos(CursoID),
     FOREIGN KEY (ProfessorID) REFERENCES Professores(ProfessorID)
 );
@@ -28,35 +27,24 @@ CREATE TABLE Matriculas (
     AlunoID INT NOT NULL,
     TurmaID INT NOT NULL,
     DataMatricula DATE NOT NULL,
- 
     FOREIGN KEY (AlunoID) REFERENCES Alunos(AlunoID),
     FOREIGN KEY (TurmaID) REFERENCES Turmas(TurmaID)
 );
--- Alunos
 INSERT INTO Alunos (Nome, DataNascimento, Email) VALUES
 ('Maria Santos', '2007-05-12', 'maria@gmail.com'),
 ('João Silva', '2006-09-23', 'joao@gmail.com');
- 
--- Professores
 INSERT INTO Professores (Nome, Especialidade) VALUES
 ('Ana Paiva', 'Matemática'),
 ('Carlos Lima', 'História');
- 
--- Cursos
 INSERT INTO Cursos (Nome, CargaHoraria) VALUES
 ('Matemática Básica', 60),
 ('História Geral', 80);
- 
--- Turmas
 INSERT INTO Turmas (CursoID, ProfessorID, AnoLetivo) VALUES
 (1, 1, 2025),
 (2, 2, 2025);
- 
--- Matrículas
 INSERT INTO Matriculas (AlunoID, TurmaID, DataMatricula) VALUES
 (1, 1, '2025-02-10'),
 (2, 2, '2025-02-11');
-
 SELECT * FROM Alunos;
 SELECT * FROM Professores;
 SELECT * FROM Cursos;
@@ -64,4 +52,5 @@ SELECT * FROM Turmas;
 SELECT * FROM Matriculas;
  
  
+
 
